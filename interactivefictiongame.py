e1046194de3b7a407ca8f1171bdb2cb369b25b77
Katskiiii <Katarina.skrzynska@student.onslow.school.nz>
2023-06-20 14:09:34 +1200

    Added introduction
    Added the introductory paragraph as a list and print it out one line at a time with the user to press enter before it prints the next line.

--- interactivefictiongame.py
+++ interactivefictiongame.py
@@ -1,3 +1,19 @@
+# introduction of game
+
+INTRO = [
+    "Welcome to 'Where in Onslow College is Irene Indiana'.",
+    "You are a new student at Onslow College eager to learn, however, another student, 'Indiana Irene' has gone missing.",
+    "Everyone is worried about her, and school cannot resume unless she is found.",
+    "Your courageous self has taken the task to solve the mystery of her disappearance.",
+    "Around Onslow are teachers who you can talk with to collect clues, but, they won’t give up their information for free.",
+    "Complete their quizzes to find new clues that will lead you closer and closer to the truth.",
+    "But beware! Someone has set up booby traps around the school to prevent you from finding Irene.",
+    "These will diminish your stamina but don’t worry because you can also find food or other items around the school that will refill your stamina.",
+    "Stamina will also be essential for completing the teachers' quizzes.",
+    "Make sure that you always have some stamina because once your stamina runs out, you’ll be sent home and won’t be able to continue your investigation.",
+    "Good luck, brave student! Find Irene and save the school!"
+]
+
 
 
 # game menu
@@ -11,6 +27,16 @@ OPTIONS = {
 
 current_row = 2  # starting row
 current_column = 2  # starting column
+
+# prints introduction
+
+for sentence in INTRO:
+    if sentence == INTRO[0]:
+        print(sentence)
+        input("*Press Enter to continue*\n")
+    else:
+        print(sentence)
+        input()
 
 # game loop
 while True:
